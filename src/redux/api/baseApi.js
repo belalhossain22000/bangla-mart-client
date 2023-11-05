3bf914512f5673680b44cf351da59e535662f275
SUMON <belalhossain22000@gmail.comgit config --global user.name>
2023-11-05 18:22:53 +0600

    live link adde api

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -3,7 +3,7 @@ import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://banglamart-server-68xt.vercel.app",
+    baseUrl: "https://y-ten-smoky.vercel.app",
   }),
   tagTypes: ['products,carts,users'],
   endpoints: (builder) => ({}),
